3aacb9a5a12832156284b39962bca5282875a0e2
Astrubale023 <105603853+Astrubale023@users.noreply.github.com>
2025-01-05 22:26:00 +0100

    Update MinecraftObjVisitor.h

--- src/Visitor/MinecraftObjVisitor.h
+++ src/Visitor/MinecraftObjVisitor.h
@@ -1,23 +1,23 @@
 #ifndef MINECRAFTOBJVISITOR_H
 #define MINECRAFTOBJVISITOR_H
 
-class Item;
-class Material;
-class Weapon;
-class Block;
-class OreBlock;
-class LightBlock;
+#include "Item.h"
+#include "Material.h"
+#include "Weapon.h"
+#include "Block.h"
+#include "OreBlock.h"
+#include "LightBlock.h"
 
 class MinecraftObjVisitor {
 public:
     virtual ~MinecraftObjVisitor() = default;
 
-    virtual void visit(Item& item) = 0;
-    virtual void visit(Material& material) = 0;
-    virtual void visit(Weapon& weapon) = 0;
-    virtual void visit(Block& block) = 0;
-    virtual void visit(OreBlock& oreBlock) = 0;
-    virtual void visit(LightBlock& lightBlock) = 0;
+    virtual void visit(const Item& item) = 0;
+    virtual void visit(const Material& material) = 0;
+    virtual void visit(const Weapon& weapon) = 0;
+    virtual void visit(const Block& block) = 0;
+    virtual void visit(const OreBlock& oreBlock) = 0;
+    virtual void visit(const LightBlock& lightBlock) = 0;
 };
 
 #endif // MINECRAFTOBJVISITOR_H
